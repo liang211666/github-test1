0be4015a31962bb245ab250d9c519685f8dcbf9d
coder0118 <lqf0118@163.com>
2023-09-01 10:23:19 +0800

    新增文件

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author lqf
+ * @Create 2023/9/1 10:20
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
